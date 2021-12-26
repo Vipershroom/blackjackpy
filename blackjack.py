e71102396f29bf3eee59fe0b0de1cf66fa88659e
Vipershroom <undertalefan812@gmail.com>
2021-12-26 00:25:26 -0500

    Created a variable to store card amount

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import random
+
+def cardDealer():
+    num = random.randint(1, 14)
+    return num
+
+print("Hello, welcome to my Blackjack game!")
+
+cardAmount = 0
+
+
+
+
+
