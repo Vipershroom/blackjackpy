88d511f8dc84bdf1e8b20a3b75cdb9d76f1abefb
Vipershroom <undertalefan812@gmail.com>
2021-12-26 00:53:29 -0500

    Finished the project

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,7 @@
 import random
 
 def cardDealer():
-    num = random.randint(1, 14)
+    num = random.randint(1, 13)
     return num
 
 print("Hello, welcome to my Blackjack game!")
@@ -10,11 +10,34 @@ cardAmount = 0
 
 print(f"You're number is currently {cardAmount}")
 
-if cardAmount < 21:
-    print("Do you want to hit? Y/N")
-    a = input()
-    if a.lower() == 'y':
-        cardAmount += cardDealer()
+running = True
+while running:
+    if cardAmount < 21:
+        print("Do you want to hit? Y/N")
+        a = input()
+        if a.lower() == 'y':
+            cardAmount += cardDealer()
+            print(f"Your current amount is {cardAmount}")
+    elif cardAmount == 21:
+        print(f"Card amount is {cardAmount}")
+        print("You Win Congratulations!")
+        
+        print("Do you want to play again? Y/N")
+        response = input()
+        if response.lower() == "n":
+            running = False
+    else:
+        print(f"Card amount is {cardAmount}")
+        print("You lose, sad!")
+        
+        print("Do you want to play again? Y/N")
+        response = input()
+        if response.lower() == "n":
+            running = False
+    
+    
+        
+        
 
 
 
