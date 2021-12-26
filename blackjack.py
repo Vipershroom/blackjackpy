53fa061107a71613826a4b2e60c42ad7052994a0
Vipershroom <undertalefan812@gmail.com>
2021-12-26 00:34:27 -0500

    Added a prompt to hit

--- blackjack.py
+++ blackjack.py
@@ -8,6 +8,15 @@ print("Hello, welcome to my Blackjack game!")
 
 cardAmount = 0
 
+print(f"You're number is currently {cardAmount}")
+
+if cardAmount < 21:
+    print("Do you want to hit? Y/N")
+    a = input()
+    if a.lower() == 'y':
+        cardAmount += cardDealer()
+
+
 
 
 
